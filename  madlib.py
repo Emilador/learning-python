bf7c06e889664c49fbc89134dd23f6651955ffb4
Emilador <108404643+Emilador@users.noreply.github.com>
2022-06-29 22:59:43 +0200

    Update  madlib.py

---  madlib.py
+++  madlib.py
@@ -1,11 +1,4 @@
 import random
-adjkt1=input("Adjektiv: ")
-adjkt2=input("Adjektiv: ")
-verb1=input("Verb: ")
-madlibs=[f"Rafas balkon is so {adjkt1} and also really {adjkt2}. Actually my uncle Rafa is {verb1} on his balkon right now."
-         ,f"My nephew Emil is better than me in {verb1}ing"]
 
-
-
-madlib=random.choice(madlibs)
-print(madlib)
+def matlib1():
+    f"Rafa is the really good in {verb1}ing but really bad in {verb2}.  "
