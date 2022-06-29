f4e9fc369df4a272831efa741c41a7d918fd2c08
Emilador <108404643+Emilador@users.noreply.github.com>
2022-06-29 15:01:51 +0200

    couple bug fixes to guess_number_game

--- guess_number_game.py
+++ guess_number_game.py
@@ -10,27 +10,39 @@ def reset_game():
     tries = 0
     zahl = (random.randint(0, 100))
     return zahl,tries
-
 while True:
     if (tries>5):
         print("You used too many tries. You fucking suck man!")
         user_inputend = str(input("You wanna play again? [yes/no]\n  "))
+        while (user_inputend!="yes")and( user_inputend!="no"):
+            user_inputend=input("Please enter [yes/no]")
         if (user_inputend == "yes"):
             zahl,tries=reset_game()
-        else:
+        elif (user_inputend=="no"):
             print("OK. Hope you come back soon")
             break
-    user_input=int(input("Please guess a number: "))
+
+    user_input = input("Please guess a number: ")
+    try:
+        user_input = float(user_input)
+    except ValueError:
+        print("That is not a number, you dumbass! Try again:")
+        continue
     tries = tries + 1
     print("Your number of tries:" ,tries)
     if (user_input==zahl) :
-        print("Wow! Very impressiv . \n")
+        print("Wow! Very impressiv. \n")
         user_inputend = str(input("You wanna play again? [yes/no]\n  "))
+        while ((user_inputend !="yes") and (user_inputend != "no")):
+            print(user_inputend)
+            user_inputend=input("Please enter [yes/no]")
         if (user_inputend=="yes") :
             zahl,tries=reset_game()
-        else:
+        elif (user_inputend=="no"):
             print("OK. Hope you come back soon")
             break
+        else:
+            print("Please say yes or no")
     elif (user_input<zahl) :
         print("Your number is a bit too small")
 
